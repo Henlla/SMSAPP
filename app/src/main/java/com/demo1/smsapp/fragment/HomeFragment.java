2d38a9eb218f1fc40ce7442accaae552b6494c93
Pham Kiet <84656881+Henlla@users.noreply.github.com>
2023-02-19 15:49:03 +0700

    commit

--- app/src/main/java/com/demo1/smsapp/fragment/HomeFragment.java
+++ app/src/main/java/com/demo1/smsapp/fragment/HomeFragment.java
@@ -2,17 +2,16 @@ package com.demo1.smsapp.fragment;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
-import androidx.annotation.RequiresApi;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import com.bumptech.glide.Glide;
 import com.demo1.smsapp.R;
@@ -37,8 +36,6 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static android.content.Context.MODE_PRIVATE;
 
 public class HomeFragment extends Fragment {
     FragmentHomeBinding fragmentHomeBinding;
--- app/src/main/java/com/demo1/smsapp/models/ApplicationType.java
+++ app/src/main/java/com/demo1/smsapp/models/ApplicationType.java
@@ -1,9 +1,69 @@
 package com.demo1.smsapp.models;
 
+import com.google.gson.annotations.SerializedName;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 public class ApplicationType {
+    @SerializedName("id")
     private int id;
+    @SerializedName("name")
     private String name;
+    @SerializedName("url")
+    private String url;
+    @SerializedName("file")
+    private String file;
+    @SerializedName("applicationById")
     private List<Application> applicationsById;
+
+    public ApplicationType(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public List<Application> getApplicationsById() {
+        return applicationsById;
+    }
+
+    public void setApplicationsById(List<Application> applicationsById) {
+        this.applicationsById = applicationsById;
+    }
+
+    @NotNull
+    @Override
+    public String toString() {
+        return name.substring(0,name.length()-5);
+    }
 }

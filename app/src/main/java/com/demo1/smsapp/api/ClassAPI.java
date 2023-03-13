25a86ad1862393fbd3322bb5c59122444be7d82d
Hoa <vongochoa30@gmail.com>
2023-03-13 14:56:31 +0700

    update timetable

--- app/src/main/java/com/demo1/smsapp/api/ClassAPI.java
+++ app/src/main/java/com/demo1/smsapp/api/ClassAPI.java
@@ -2,13 +2,15 @@ package com.demo1.smsapp.api;
 
 import com.demo1.smsapp.dto.ResponseModel;
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Header;
-import retrofit2.http.Path;
+import retrofit2.http.*;
 
 public interface ClassAPI {
 
     @GET("getClass/{classId}")
     Call<ResponseModel> getClassById(@Header("Authorization")String _token, @Path("classId")Integer classId);
+
+    @POST("findClassCode")
+    @FormUrlEncoded
+    Call<ResponseModel> findClassCode(@Header("Authorization")String _token, @Field("classCode") String classCode);
 
 }
